e8e8cb94433bf7fb84b85185891bac8c3c174cfe
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-23 19:59:43 -0600

    feat: add stock lookup to get data from iex api

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,9 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(_ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_api_key],
+                                  secret_token: Rails.application.credentials.iex_client[:secret_api_key],
+                                  endpoint: 'https://cloud.iexapis.com/v1')
+
+    client.quote(_ticker_symbol).latest_price
+  end
 end
