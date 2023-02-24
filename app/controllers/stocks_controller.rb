7932d2d915cce23c9b084a22f57e31d4a223b4bf
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-24 12:18:03 -0600

    feat: render from ajax the results

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,29 @@
 class StocksController < ApplicationController
+  before_action :present_params, only: %i[search]
+
   def search
-    stock = Stock.new_lookup(params[:stock])
-    render json: stock
+    @stock = Stock.new_lookup(params[:stock])
+
+    if @stock
+      respond_to { |format| format.js { render partial: "users/result" } }
+    end
+
+    unless @stock
+      respond_to do |format|
+        flash.now[:alert] = "Not found, please another symbol"
+        format.js { render partial: "users/result" }
+      end
+    end
+  end
+
+  private
+
+  def present_params
+    unless params[:stock].present?
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a symbol to search"
+        format.js { render partial: "users/result" }
+      end
+    end
   end
 end
