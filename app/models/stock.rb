58119aa39752fad7422c5f4234a505239d12caa6
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-24 12:18:27 -0600

    feat: adding begin rescue bloq in the API call

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,21 @@
 class Stock < ApplicationRecord
   def self.new_lookup(_ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:publishable_api_key],
-                                  secret_token: Rails.application.credentials.iex_client[:secret_api_key],
-                                  endpoint: 'https://cloud.iexapis.com/v1')
+    client =
+      IEX::Api::Client.new(
+        publishable_token:
+          Rails.application.credentials.iex_client[:publishable_api_key],
+        secret_token: Rails.application.credentials.iex_client[:secret_api_key],
+        endpoint: "https://cloud.iexapis.com/v1"
+      )
 
-    client.quote(_ticker_symbol).latest_price
+    begin
+      new(
+        ticker: _ticker_symbol,
+        name: client.company(_ticker_symbol).company_name,
+        last_price: client.quote(_ticker_symbol).latest_price
+      )
+    rescue => exception
+      return nil
+    end
   end
 end
