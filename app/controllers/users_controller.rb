9851b4009e06e7bc968648f4f3c458c68e900a35
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-24 22:03:45 -0600

    feat: adding show and refactor the search methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
+  before_action :require_friend, only: %i[search]
+
   def my_portfolio
+    @user = current_user
     @tracked_stocks = current_user.stocks
   end
 
@@ -7,7 +10,37 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
+  def show
+    @user = User.find(params[:id])
+    @tracked_stocks = @user.stocks
+  end
+
   def search
-    render json: params[:friend]
+    @friend = User.search(params[:friend])
+    @friend = current_user.except_current_user(@friend)
+
+    if @friend
+      respond_to do |format|
+        format.js { render partial: "users/friend_result" }
+      end
+    end
+
+    unless @friend
+      respond_to do |format|
+        flash.now[:alert] = "Couldn't find user"
+        format.js { render partial: "users/friend_result" }
+      end
+    end
+  end
+
+  private
+
+  def require_friend
+    unless params[:friend].present?
+      respond_to do |format|
+        flash.now[:alert] = "Please enter a name or email to search"
+        format.js { render partial: "users/friend_result" }
+      end
+    end
   end
 end
