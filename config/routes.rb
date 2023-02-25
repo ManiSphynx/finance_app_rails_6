ab112ed4826e54b446897d5a5646b43078267aa4
Emanuel Romero Madrigal <emanuel.romero@hotmail.es>
2023-02-24 22:02:56 -0600

    feat: generate friendships routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   get "search_stocks", to: "stocks#search"
   get "my_friends", to: "users#my_friends"
   get "search_friend", to: "users#search"
+  resources :friendships, only: %i[create destroy]
+  resources :users, only: %i[show]
 end
